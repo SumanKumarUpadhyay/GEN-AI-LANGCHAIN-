bde60d183874d32e0e3f86f71af080ea1481cee2
Suman kumar <sumankumarpanditpur520@gmail.com>
2025-10-09 16:01:01 +0530

    Add files via upload

---
+++ 2.) Chatmodels/4_chatmodel_hf_api.py
@@ -0,0 +1,18 @@
+import os
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+    temperature=0.7,
+    max_new_tokens=256,
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("Who is the prime minister of india now ?,who is MSD and virat kohli ?")
+
+print(result.content)
